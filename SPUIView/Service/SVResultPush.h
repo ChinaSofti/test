15bedd11803d6a4b11243c07aafa6e8020fb84ad
徐瑞 <xzhyxr@163.com>
2016-03-16 09:28:20 +0800

    去掉无用的头文件

--- SPUIView/Service/SVResultPush.h
+++ SPUIView/Service/SVResultPush.h
@@ -7,7 +7,6 @@
 //
 
 #import "SVCurrentResultModel.h"
-#import "SVWarningView.h"
 #import <Foundation/Foundation.h>
 
 @interface SVResultPush : NSObject <NSURLConnectionDelegate, NSURLConnectionDataDelegate, UIAlertViewDelegate>
