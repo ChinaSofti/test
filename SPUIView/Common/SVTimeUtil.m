ab3274e6441a3ceb42582d8d77231d56ec939bb8
WBapple <459423429@qq.com>
2016-04-16 17:16:39 +0800

    日期格式化英文状态出现汉字问题的解决

--- SPUIView/Common/SVTimeUtil.m
+++ SPUIView/Common/SVTimeUtil.m
@@ -41,12 +41,30 @@
  *  @param formatStr 日期格式
  *  @return 日期字符串
  */
+//+ (NSString *)formatDateByMilliSecond:(long long)timeNum formatStr:(NSString *)formatStr
+//{
+//    NSNumber *time = [NSNumber numberWithLongLong:(timeNum / 1000)];
+//    NSDate *nd = [NSDate dateWithTimeIntervalSince1970:[time doubleValue]];
+//
+//    NSDateFormatter *dateFormat = [[NSDateFormatter alloc] init];
+//    [dateFormat setDateFormat:formatStr];
+//    NSString *dateString = [dateFormat stringFromDate:nd];
+//    return dateString;
+//}
 + (NSString *)formatDateByMilliSecond:(long long)timeNum formatStr:(NSString *)formatStr
 {
     NSNumber *time = [NSNumber numberWithLongLong:(timeNum / 1000)];
-    NSDate *nd = [NSDate dateWithTimeIntervalSince1970:[time doubleValue]];
-
     NSDateFormatter *dateFormat = [[NSDateFormatter alloc] init];
+    SVI18N *i18n = [SVI18N sharedInstance];
+    NSString *language = [i18n getLanguage];
+    
+    if ([language isEqual:@"en"])
+    {
+        NSLog (@"!!!!!!!!!!!!");
+        dateFormat.locale = [[NSLocale alloc] initWithLocaleIdentifier:@"en_US"];
+    }
+    NSDate *nd = [NSDate dateWithTimeIntervalSince1970:[time doubleValue]];
+    
     [dateFormat setDateFormat:formatStr];
     NSString *dateString = [dateFormat stringFromDate:nd];
     return dateString;
