3af4f2e56cbc40cd6d6d6f2f7c64a17efb9ae292
徐瑞 <xzhyxr@163.com>
2016-04-26 10:01:00 +0800

    当下载速度是0时会导致闪退，除数不能是0

--- SPUIView/View/UIUtils/SVChart.m
+++ SPUIView/View/UIUtils/SVChart.m
@@ -11,7 +11,7 @@
 @implementation SVChart
 {
     SVLineChart *_lineChart;
-    
+
     NSMutableArray *_arrays;
     CGFloat _maxY;
 }
@@ -22,12 +22,12 @@
     self = [super initWithFrame:CGRectMake (0, 0, size.width, size.height)];
     if (self)
     {
-        _maxY = 0;
+        _maxY = 0.1;
         self.clipsToBounds = YES;
         _lineChart = [[SVLineChart alloc] initWithFrame:self.frame maxY:_maxY];
         [self addSubview:_lineChart];
         [showOnView addSubview:self];
-        
+
         _arrays = [[NSMutableArray alloc] init];
     }
 
@@ -37,17 +37,20 @@
 - (void)addValue:(float)value
 {
     [_lineChart addValue:value];
-    
+
     [_arrays addObject:[NSString stringWithFormat:@"%f", value]];
-    
-    if (_maxY * 0.8 < value) {
+
+    if (_maxY * 0.8 < value)
+    {
         _maxY = value / 0.8;
-        for (UIView *view in self.subviews) {
+        for (UIView *view in self.subviews)
+        {
             [view removeFromSuperview];
         }
         _lineChart = [[SVLineChart alloc] initWithFrame:self.frame maxY:_maxY];
         [self addSubview:_lineChart];
-        for (int i = 0; i < _arrays.count; i++) {
+        for (int i = 0; i < _arrays.count; i++)
+        {
             [_lineChart addValue:[_arrays[i] floatValue]];
         }
     }
