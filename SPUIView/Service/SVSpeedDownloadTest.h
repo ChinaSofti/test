afd9c59560390ebadd4f733bb6250ab209ef5ce1
徐瑞 <xzhyxr@163.com>
2016-04-29 16:45:58 +0800

    修改带宽测试，改为两个线程一起测试

--- SPUIView/Service/SVSpeedDownloadTest.h
+++ SPUIView/Service/SVSpeedDownloadTest.h
@@ -6,23 +6,22 @@
 //  Copyright © 2016年 Huawei. All rights reserved.
 //
 
-#import "SVSpeedTest.h"
-#import "SVSpeedTestResult.h"
 #import <Foundation/Foundation.h>
 
 @interface SVSpeedDownloadTest : NSObject <NSURLConnectionDelegate, NSURLConnectionDataDelegate>
 
-- (id)initWithUrl:(NSURL *)url
-     WithDelegate:(id)currDelegate
-       WithTestId:(long long)testId
-   WithTestResult:(SVSpeedTestResult *)testResult
-   WithTestContet:(SVSpeedTestContext *)testContext
-   WithTestStatus:(TestStatus)status;
+- (id)initWithUrl:(NSURL *)url WithTestStatus:(TestStatus)status;
 
 // 开始测试
 - (void)startTest;
 
 // 更新测试状态
 - (void)updateStatus:(TestStatus)status;
+
+// 更新内部测试状态
+- (void)updateInnerStatus:(TestStatus)status;
+
+// 获取下载大小
+- (long long)getSize;
 
 @end
