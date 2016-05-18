8196da027102c57ab660df570551d9617fd42f4f
WBapple <459423429@qq.com>
2016-05-18 10:20:29 +0800

    分享界面重写,(大图片换成了小图片的拼接,为了省内存) 关于界面和分享界面无用图片删除 分享文字描边类添加

---
+++ SPUIView/Tool/CTWBLabel.h
@@ -0,0 +1,13 @@
+//
+//  CTWBLabel.h
+//  SpeedPro
+//
+//  Created by WBapple on 16/5/17.
+//  Copyright © 2016年 Huawei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CTWBLabel : UILabel
+
+@end
